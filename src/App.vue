dd1181dc655604e821cc5d1c378d14a49b0278a1
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-04 23:36:09 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import FooterSection from './components/section/FooterSection.vue';
 import HeaderSection from './components/section/HeaderSection.vue';
 import HeroSection from './components/section/HeroSection.vue';
+import SignatureDishes from './components/section/SignatureDishes.vue';
 
 </script>
 
 <template>
   <HeaderSection />
   <HeroSection/>
+  <!-- <SignatureDishes/> -->
   <FooterSection />
 </template>
 
