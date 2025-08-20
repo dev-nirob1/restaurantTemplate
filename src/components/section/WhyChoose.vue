018494764780e72e7f9401b02715cc6a8845551b
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-20 23:23:57 +0600

    update whychooseUs

--- src/components/section/WhyChoose.vue
+++ src/components/section/WhyChoose.vue
@@ -6,11 +6,11 @@ import ListItem from '../Elements/ListItem.vue';
 
 <template>
   <!-- ======== Why Choose Us section ======== -->
-  <section class="statistics">
+  <section class="statistics bg-light">
     <div class="container">
       <ul class="small-2 larger-3 justify-center align-center gap-2">
         <!-- Dishes Served -->
-        <ListItem>
+        <ListItem class="flex-center">
           <i class="fa-solid fa-utensils fa-3x"></i>
           <div>
             <BaseTitle>15k+</BaseTitle>
@@ -19,7 +19,7 @@ import ListItem from '../Elements/ListItem.vue';
         </ListItem>
 
         <!-- Happy Customers -->
-        <ListItem>
+        <ListItem class="flex-center">
           <i class="fa-solid fa-smile-beam fa-3x"></i>
           <div>
             <BaseTitle>10k+</BaseTitle>
@@ -28,7 +28,7 @@ import ListItem from '../Elements/ListItem.vue';
         </ListItem>
 
         <!-- Years of Culinary Excellence -->
-        <ListItem>
+        <ListItem class="flex-center">
           <i class="fa-solid fa-award fa-3x"></i>
           <div>
             <BaseTitle>20+</BaseTitle>
@@ -44,16 +44,12 @@ import ListItem from '../Elements/ListItem.vue';
 <style scoped>
 .statistics {
   padding: 3.75rem 0;
-  background: var(--light-color);
 }
 .statistics ul {
   padding: 0;
 }
 
 .statistics ul li {
-  display: flex;
-  align-items: center;
-  justify-content: center;
   gap: 1rem;
   padding: 2rem .5rem;
   background-color: var(--white-color);
