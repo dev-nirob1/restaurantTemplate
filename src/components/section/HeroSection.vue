ee720194d8a01df0cd4f449d95c30c27a28e68c0
dev-nirob1 <dev.nirob1@gmail.com>
2025-08-20 23:21:23 +0600

    btn styles moved to main.css and update hero section

--- src/components/section/HeroSection.vue
+++ src/components/section/HeroSection.vue
@@ -6,13 +6,13 @@ import HeroTitle from '../Elements/HeroTitle.vue';
 </script>
 
 <template>
-    <section class="hero">
+    <section class="hero flex-center">
         <div class="content">
             <HeroTitle>Savor the Art of Dining</HeroTitle>
             <BaseParagraph>
                 Where every dish tells a story, crafted with locally-sourced ingredients.
             </BaseParagraph>
-            <div class="flex justify-center gap-1">
+            <div class="flex-center gap-1">
                 <BaseButton class="bg-alternative">Reserve a Table</BaseButton>
                 <BaseButton class="outline">Explore Menu</BaseButton>
             </div>
@@ -21,53 +21,19 @@ import HeroTitle from '../Elements/HeroTitle.vue';
 </template>
 <style scoped>
 .hero {
-    font-family: 'Montserrat', sans-serif;
     min-height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
     text-align: center;
     color: var(--white-color);
-    background: linear-gradient(to bottom, rgb(from var(--primary-color) r g b / 60%), rgb(from var(--primary-color) r g b / 50%)), url('https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80') center / cover no-repeat;
-}
-
-.hero p {
-    font-size: 1.25rem;
-}
-
-/* buttons styles  */
-.hero .btn {
-    color: var(--white-color);
-    border-radius: 2rem;
-    padding: .75rem 1rem;
-}
-
-.hero .btn.outline {
-    border-color: var(--secondary-color);
-    color: var(--white-color);
-}
-
-.hero .btn.outline:hover {
-    background: rgb(from var(--primary-color) r g b / 30%);
-    transform: translateY(-.25rem);
-    border-color: var(--secondary-color);
-    color: var(--secondary-color);
-}
-
-.hero .btn.bg-alternative {
-    border-color: var(--alternative-color);
-}
-
-.hero .btn.bg-alternative:hover {
-    transform: translateY(-.25rem);
-    color: var(--primary-color);
-    border-color: var(--secondary-color);
-    background-color: var(--secondary-color);
+    background: linear-gradient(to bottom, rgb(from var(--primary-color) r g b / 60%), rgb(from var(--primary-color) r g b / 50%)), url('https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80') center / cover no-repeat fixed;
 }
 
 @media (min-width: 768px) {
     .hero .content {
         max-width: 80%;
+    }
+
+    .hero p {
+        font-size: 1.25rem;
     }
 
     .hero .btn {
